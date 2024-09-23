173b2be255f6dc2e763f3007e99eaac4b5da8cf6
Håkon Gulliksrud <haakon514@gmail.com>
2024-09-23 16:33:04 +0200

    init commit

---
+++ src/main/java/com/example/microservices2024hagu021/Microservices2024Hagu021Application.java
@@ -0,0 +1,13 @@
+package com.example.microservices2024hagu021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices2024Hagu021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices2024Hagu021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservices2024hagu021/Microservices2024Hagu021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices2024hagu021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservices2024Hagu021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
